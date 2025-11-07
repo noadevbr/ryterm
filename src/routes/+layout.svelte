e797804e07ccfadade8f7f03e5bd16f4906d6d81
AuryChar <nat4nz007love@gmail.com>
2025-11-07 18:27:49 -0300

    add header and footer to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	let { children } = $props()
 
 	import "@/app.css"
+	import Footer from "@/components/Footer.svelte";
+	import Header from "@/components/Header.svelte";
 </script>
 
+<Header title={"Ryterm"}></Header>
 {@render children()}
+<Footer></Footer>
